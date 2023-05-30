182680b5b1d3c499d069b595d4eae8d80e63139e
gzdgul <gzdgul00@gmail.com>
2023-05-31 00:47:30 +0300

    modified: viewport h-w

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ import { BrowserRouter , Routes, Route, Switch} from 'react-router-dom';
 import Register from "./pages/register";
 import Home from "./pages/home";
 import Chat from "./pages/chat";
+import {useEffect} from "react";
 
 
 function App() {
-
+useEffect(() => {
+    // viewport genişliği ve yüksekliği al
+    let viewportWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+    let viewportHeight = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+    // viewport genişliği ve yüksekliği üzerinde değişiklik yap
+    document.documentElement.style.setProperty('--viewport-width', viewportWidth + 'px');
+    document.documentElement.style.setProperty('--viewport-height', viewportHeight + 'px');
+},[])
     return (
         <>
             <Routes>
